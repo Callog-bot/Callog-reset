6b38f9b657d2ad0ef28ba6f867b27717dd36ea6d
Daniel <danihtoledo22@gmail.com>
2021-06-10 10:26:05 -0300

    Fix ( #2 )

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const shell = require('shelljs');
 require('dotenv').config();
 
-shell.exec(`git clone "${process.env.REPO_URL}"`);
-shell.exec('Callog-bot');
-shell.exec(`${process.env.PWD}`);
+const wait = (seconds, cb) => {
+  return new Promise(
+    setInterval(() => {
+      cb();
+      Promise.resolve();
+    }, seconds * 1000)
+  );
+};
 
-setTimeout(()=> {
+async function main() {
+  shell.exec(`git clone "${process.env.REPO_URL}"`);
+  await wait(5, shell.exec('cd Callog-reset'));
+  shell.exec(`git config --global user.email "${process.env.EMAIL}"`);
+  shell.exec(`git config --global user.name "${process.env.USER}"`);
   shell.exec('git commit --allow-empty -m reset');
-}, 10000)
+  shell.exec('git push origin main:main');
+  await wait(1, shell.exec(`${process.env.USER}`));
+  await wait(1, shell.exec(`${process.env.PWD}`));
+}
+main();
