532ac10611f9f0ef84719ee703b8da6e242d1e5c
Daniel <danihtoledo22@gmail.com>
2021-06-10 10:44:07 -0300

    Fix ( #4 )

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const sleep = (sec) => {
 
 async function main() {
   shell.exec(`git clone "${process.env.REPO_URL}"`);
+  await sleep(1);
+  shell.exec(`${process.env.USER}`);
+  await sleep(1);
+  shell.exec(`${process.env.PWD}`);
   await sleep(5);
   shell.exec('cd Callog-reset');
   shell.exec(`git config --global user.email "${process.env.EMAIL}"`);
   shell.exec(`git config --global user.name "${process.env.USER}"`);
   shell.exec('git commit --allow-empty -m reset');
   shell.exec('git push origin main:main');
-  await sleep(1);
-  shell.exec(`${process.env.USER}`);
-  await sleep(1);
-  shell.exec(`${process.env.PWD}`);
 }
 main();
